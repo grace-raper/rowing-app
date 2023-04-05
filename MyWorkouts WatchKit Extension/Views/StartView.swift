dbe0c37a753f12e5e77371b84cc4ff6fda496953
grace-raper <graceraper@gmail.com>
2023-04-04 17:06:17 -0700

    changed workout options to Outdoor Row or Indoor Erg

--- MyWorkouts WatchKit Extension/Views/StartView.swift
+++ MyWorkouts WatchKit Extension/Views/StartView.swift
@@ -10,13 +10,14 @@ import HealthKit
 
 struct StartView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
-    var workoutTypes: [HKWorkoutActivityType] = [.cycling, .running, .walking]
-
+    
+    var workoutTypes: [HKWorkoutActivityType] = [.rowing, .paddleSports]
+    
     var body: some View {
         List(workoutTypes) { workoutType in
             NavigationLink(workoutType.name, destination: SessionPagingView(),
-                           tag: workoutType, selection: $workoutManager.selectedWorkout)
-                .padding(EdgeInsets(top: 15, leading: 5, bottom: 15, trailing: 5))
+                           tag: .rowing, selection: $workoutManager.selectedWorkout)
+                .padding(EdgeInsets(top: 30, leading: 5, bottom: 30, trailing: 5))
         }
         .listStyle(.carousel)
         .navigationBarTitle("Workouts")
@@ -43,8 +44,10 @@ extension HKWorkoutActivityType: Identifiable {
             return "Run"
         case .cycling:
             return "Bike"
-        case .walking:
-            return "Walk"
+        case .paddleSports:
+            return "Indoor Erg"
+        case .rowing:
+            return "Outdoor Row"
         default:
             return ""
         }
